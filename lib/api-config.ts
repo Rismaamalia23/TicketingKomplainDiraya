ee66ef25c55c23a7b53a0f00723bda19a5185e27
Rismaamalia23 <2311103031@ittelkom-pwt.ac.id>
2026-02-13 15:43:37 +0700

    Configure Monorepo Deployment to Vercel (Frontend + Backend)

--- lib/api-config.ts
+++ lib/api-config.ts
@@ -1,2 +1,2 @@
 const isProd = process.env.NODE_ENV === 'production';
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || (isProd ? '/api-server' : 'http://127.0.0.1:5900/api');
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || (isProd ? '/api' : 'http://127.0.0.1:5900/api');
